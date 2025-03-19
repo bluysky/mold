fbcdfee717b24056c7df016f18203112a5f4a94a
bluysky <50596811+bluysky@users.noreply.github.com>
2025-03-19 13:26:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Anon Key:', supabaseAnonKey);
 
 // 언어 전환
 let language = "en";

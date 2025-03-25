7b14d85ef5b0ff2a0904bccbdfe36f34f0274b17
bluysky <50596811+bluysky@users.noreply.github.com>
2025-03-25 16:55:59 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import MoldList from './components/MoldList';
+import MoldCreate from './components/MoldCreate';
+import MoldCreate from './components/MoldEdit';
 import { supabase } from './supabaseClient';
 
 function App() {

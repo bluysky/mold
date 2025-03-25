0510800877c37eefd6492c587f4986b31ee8124d
bluysky <50596811+bluysky@users.noreply.github.com>
2025-03-25 17:43:12 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 // src/App.js
-// import React from 'react';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import MoldList from './components/MoldList';
 import MoldCreate from './components/MoldCreate';
-import MoldEdit from './components/MoldEdit';
+import MoldEdit from './components/MoldEdit'; // Import 수정
 import { supabase } from './supabaseClient';
 
 function App() {
@@ -22,10 +21,10 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/mold"> {/* basename prop 추가 */}
       <Routes>
-        <Route path="/" element={session ? <Navigate to="/mold-list" /> : <Login />} />
-        <Route path="/mold-list" element={session ? <MoldList /> : <Navigate to="/" />} />
+        <Route path="/" element={session ? <Navigate to="/mold-list" replace /> : <Login />} />
+        <Route path="/mold-list" element={session ? <MoldList /> : <Navigate to="/" replace />} />
         <Route path="/mold-create" element={<MoldCreate />} />
         <Route path="/mold-edit/:id" element={<MoldEdit />} />
       </Routes>

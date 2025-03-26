0aa24bcce733c9dc634020a95137223d84af7df0
bluysky <50596811+bluysky@users.noreply.github.com>
2025-03-26 03:45:54 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
+import SignUp from './components/SignUp'; // SignUp 컴포넌트 import
 import MoldList from './components/MoldList';
 import MoldCreate from './components/MoldCreate';
 import MoldEdit from './components/MoldEdit';
@@ -25,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} /> {/* 초기 접속 시 /login으로 리다이렉트 */}
         <Route path="/login" element={<Login />} /> {/* /login 경로에 Login 컴포넌트 연결 */}
+        <Route path="/signup" element={<SignUp />} /> {/* 회원가입 라우트 추가 */}
         <Route path="/mold-list" element={session ? <MoldList /> : <Navigate to="/login" replace />} /> {/* 로그인 안된 경우 /login으로 리다이렉트 */}
         <Route path="/mold-create" element={<MoldCreate />} />
         <Route path="/mold-edit/:id" element={<MoldEdit />} />
